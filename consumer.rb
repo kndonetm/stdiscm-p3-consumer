d0b0fe552bff025c15262b93419e0e6d485985ee
kndone <justinlim4916@gmail.com>
2025-04-02 16:33:15 +0800

    feat: send OK messages to signal producer when action is successful

--- consumer.rb
+++ consumer.rb
@@ -10,12 +10,26 @@ class VideoServer
         @video_directory = settings["VIDEO_DIRECTORY"]
     end
 
+    def receive_video(size, socket)
+        data = socket.read size
+        puts "Received video of size #{size} from client"
+        send_ok socket
+        return data
+    end
+
     def receive_command(socket)
         size = socket.read(8)
         if not size then return end
         size = size.unpack1('q')
-        puts size
-        return socket.read(size)
+        cmd = socket.read(size)
+        puts "Received command #{cmd}"
+        send_ok socket
+        return cmd
+    end
+
+    def send_ok(socket)
+        puts "Sent OK"
+        socket.write("OK")
     end
 
     def handle_client(client)
@@ -23,11 +37,9 @@ class VideoServer
         
         while raw_cmd = receive_command(client) do 
             cmd = JSON.parse(raw_cmd)
-        
-            puts cmd
             
             if cmd["action"] == "sendFile" then
-                data = client.read(cmd["size"])
+                data = receive_video cmd["size"], client
         
                 filepath = File.join(@video_directory, cmd["filename"])
                 file = File.new(filepath, "wb") 
@@ -43,7 +55,7 @@ class VideoServer
 
     def mainloop
         @server = TCPServer.open(@bound_addr, @port)
-        puts "Listening on port #{@port}"
+        puts "Listening on port #{@port} with bound address #{@bound_addr}"
 
         loop do
             Thread.start(@server.accept) do |client| self.handle_client(client) end
