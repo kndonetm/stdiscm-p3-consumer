5bd6b6cb439e3798f922b6b3fbc0c52a674eb8b6
kndone <justinlim4916@gmail.com>
2025-04-03 18:31:00 +0800

    chore: merge

--- consumer.rb
+++ consumer.rb
@@ -5,7 +5,7 @@ class VideoServer
     attr_reader :port, :server
 
     def initialize(settings)
-        @port = settings["PORT"]
+        @port = settings["PORT"].to_i
         @bound_addr = settings["BOUND_ADDR"]
         @video_directory = settings["VIDEO_DIRECTORY"]
         @num_threads = settings["NUM_CONSUMER_THREADS"].to_i
@@ -18,24 +18,84 @@ class VideoServer
                 worker_thread id
             end
             
-            thread["status"] = "idle"
+            thread["status"] = ["idle"]
+            thread["status"].extend(MonitorMixin)
+            thread["status_cond"] = thread["status"].new_cond
+
+            thread["port"] = @port + i + 1
 
             @threads << thread
         end
 
         @queue = []
+
+        @ready_to_accept = []
+        @ready_to_accept.extend(MonitorMixin)
+        @ready_to_accept_cond = @ready_to_accept.new_cond
     end
 
     def worker_thread(id)
         puts "Created consumer thread with id #{id}"
+        
+        loop do
+            Thread.current["status"].synchronize do
+                Thread.current["status_cond"].wait_while { Thread.current["status"][0] == "idle" }
+            end
 
-        loop do end
+            num_videos = Thread.current["num_videos"]
+            producer_ip = Thread.current["ip"]
+            request_id = Thread.current["request_id"]
+
+            puts "Host thread #{id} assigned to receive #{num_videos} videos from producer #{producer_ip} thread #{request_id}"
+
+            @ready_to_accept.synchronize do
+                @ready_to_accept << Thread.current
+                @ready_to_accept_cond.signal
+            end
+
+            puts "Thread #{id} opening TCP server to #{producer_ip} on port #{Thread.current["port"]}"
+            server = TCPServer.open(producer_ip, Thread.current["port"])
+            client = server.accept
+            
+            num_videos.times do 
+                cmd = receive_json(client)
+                if cmd["action"] == "sendFile" then
+                    receive_video cmd["size"], client, File.join(@video_directory, cmd["filename"])
+                end
+            end
+
+            client.close
+            server.close
+
+            set_thread_status Thread.current, "idle"
+        end
+    end
+
+    def set_thread_status(thread, status)
+        thread["status"][0] = status
+    end
+
+    def assign_thread(thread, num_videos, ip, request_id)
+        thread["status"].synchronize do 
+            thread["num_videos"] = num_videos
+            thread["ip"] = ip
+            thread["request_id"] = request_id
+            set_thread_status thread, "active"
+            thread["status_cond"].signal
+        end
     end
 
     def get_free_threads
         @threads.each_with_index.select { |thread, id|
-            thread["status"] == "idle" 
+            thread["status"][0] == "idle" 
         }
+    end
+
+    def get_num_videos_in_queue
+        @queue.map {|ip, id, num_videos|
+            num_videos
+        }
+        .sum
     end
 
     def receive_video(size, socket, filepath)
@@ -57,30 +117,41 @@ class VideoServer
         size = socket.read(8)
         if not size then return end
         size = size.unpack1('q')
-        cmd = socket.read(size)
-        puts "Received #{cmd}"
+        json = socket.read(size)
+        puts "Received #{json}"
         send_ok socket
-        return cmd
+        return JSON.parse(json)
+    end
+
+    def send_json(msg, socket)
+        puts "Attempting to send #{msg} to #{socket.local_address.ip_address}"
+        socket.write [msg.length].pack("q")
+        socket.write msg
     end
 
     def send_ok(socket)
-        puts "Sent OK"
         socket.write("OK")
     end
 
-    def get_num_videos_in_queue
-        @queue.map {|ip, id, num_videos|
-            num_videos
-        }
-        .sum
+    def request_threads_response(assigned_requests, queued_requests)
+        return JSON.generate({
+            assigned: assigned_requests,
+            queued: queued_requests
+        })
+    end
+
+    def thread_ready_response(producer_thread_id, port)
+        return JSON.generate({
+            id: producer_thread_id,
+            port: port
+        })
     end
 
     def handle_client(client)
         client_ip = client.local_address.ip_address
         puts "Accepting client from #{client_ip}"
         
-        while raw_cmd = receive_json(client) do 
-            cmd = JSON.parse(raw_cmd)
+        while cmd = receive_json(client) do 
             
             if cmd["action"] == "sendFile" then
                 receive_video cmd["size"], client, File.join(@video_directory, cmd["filename"])
@@ -94,6 +165,7 @@ class VideoServer
                 assigned_threads = free_threads[0, num_assigned_threads]
                 assigned_threads.each_with_index.each do |thread_info, request_id|
                     id = thread_info[1]
+                    assign_thread @threads[id], thread_requests[request_id], client_ip, request_id
                     puts "Assigned thread #{id} to request ID #{request_id}"
                 end
 
@@ -128,11 +200,18 @@ class VideoServer
                     end
                 end
 
-                puts "Assigned requests: #{assigned_requests}"
-                puts "Queued requests: #{queued_requests}"
+                send_json request_threads_response(assigned_requests, queued_requests), client
 
+                assigned_requests.each do
+                    @ready_to_accept.synchronize do
+                        @ready_to_accept_cond.wait_while { @ready_to_accept.empty? }
+                        ready_thread = @ready_to_accept.shift
+                        send_json thread_ready_response(ready_thread["request_id"], ready_thread["port"]), client
+                    end
+                end
 
             elsif cmd["action"] == "exit" then
+                puts "Connection closed by client #{client_ip}"
                 client.close
                 return
             end
