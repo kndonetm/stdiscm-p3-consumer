69377e66fdbde9eb8504072e5f17e52fd45b2db0
kndone <justinlim4916@gmail.com>
2025-04-11 19:54:49 +0800

    fix: preview not showing up

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,19 +1,19 @@
 # consumer/app/controllers/videos_controller.rb
 class VideosController < ApplicationController
   def index
-    @videos = Dir.glob(Rails.root.join("consumer", "public/videos", "*.mp4")).map do |path|
+    @videos = Dir.glob(Rails.root.join("public/received_videos", "*.mp4")).map do |path|
       {
         name: File.basename(path)[32..-1],  # skip video hash
-        filename: URI.encode_www_form_component(File.basename(path)), #added this
-        
-        url: "/#{URI.encode_www_form_component(File.basename(path))}",
+        filename: URI.encode_www_form_component(File.basename(path)), # added this
+
+        url: "received_videos/#{URI.encode_www_form_component(File.basename(path))}",
         # size: number_to_human_size(File.size(path)),
         created_at: File.ctime(path)
       }
     end.sort_by { |v| v[:created_at] }.reverse
   end
 
-  #added
+  # added
   def stream
     filename = "#{URI.decode_www_form_component(params[:name])}#{'.mp4' unless params[:name].ends_with?('.mp4')}"
 
@@ -23,10 +23,9 @@ class VideosController < ApplicationController
     Rails.logger.info "Resolved path: #{path}"
 
     if File.exist?(path)
-      send_file path, type: 'video/mp4', disposition: 'inline'
+      send_file path, type: "video/mp4", disposition: "inline"
     else
       render plain: "Video not found", status: :not_found
     end
   end
-
 end
